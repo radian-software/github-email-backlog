11fbb34de3ee18e4c691224b52ca9dfe731ed426
Radon Rosborough <radon.neon@gmail.com>
2020-06-09 08:40:21 -0600

    The whole shebang

--- content_script.js
+++ content_script.js
@@ -1,4 +1,4 @@
-const RUN_PERIOD_MS = 5000; //6 * 3600 * 1000;
+const RUN_PERIOD_MS = 6 * 3600 * 1000;
 
 async function shouldRun() {
   const { timestamp: lastTimestamp } = await new Promise((resolve) =>
@@ -12,8 +12,18 @@ async function shouldRun() {
     await new Promise((resolve) =>
       chrome.storage.sync.set({ timestamp: curTimestamp }, resolve)
     );
+    console.log(
+      `Running as it has been ${
+        curTimestamp - lastTimestamp
+      }ms since the last run`
+    );
     return true;
   } else {
+    console.log(
+      `Not running as it has only been ${
+        curTimestamp - lastTimestamp
+      }ms since the last run`
+    );
     return false;
   }
 }
@@ -108,21 +118,97 @@ function countUnreadFraction(notifications) {
 }
 
 async function getAllNotifications({ token }) {
+  console.log("Fetching notifications (page 1) ...");
   let curPage = await getNotifications({ token });
   let all = curPage;
   let ids = new Set(all.map((n) => n.id));
+  let idx = 2;
   while (countUnreadFraction(curPage) > 0.1) {
     const before = curPage[curPage.length - 1].updated_at;
+    console.log(`Fetching notifications (page ${idx}) ...`);
     curPage = await getNotifications({ token, before });
     for (const n of curPage) {
       if (!ids.has(n.id)) {
         all.push(n);
       }
     }
+    idx += 1;
   }
   return all;
 }
 
-chrome.storage.sync.get(["token"], ({ token }) => {
-  getAllNotifications({ token }).then(console.log).catch(console.error);
-});
+function estimateResponseTimeDays(notifications) {
+  const percentile =
+    notifications[
+      Math.floor(countUnreadFraction(notifications) * notifications.length)
+    ];
+  const age = new Date().getTime() - new Date(percentile.updated_at).getTime();
+  return (age / 86400 / 1000) * 2;
+}
+
+function getStatus(numDays) {
+  if (numDays >= 3) {
+    return {
+      message: `Estimated inbox backlog: about ${Math.floor(numDays)} days`,
+      emoji: "inbox_tray",
+      busy: true,
+    };
+  } else {
+    return {
+      message: "I typically reply in about 1 day",
+      emoji: "kiwi_fruit",
+      busy: false,
+    };
+  }
+}
+
+async function pingWebhook(webhook) {
+  const resp = await fetch(webhook);
+  if (resp.ok) {
+    console.log("Notified webhook");
+  } else {
+    console.log(
+      `Got error response from webhook: ${resp.status} ${resp.statusText}`
+    );
+  }
+}
+
+async function updateStatus() {
+  const { token: apiToken } = await new Promise((resolve) =>
+    chrome.storage.sync.get(["token"], resolve)
+  );
+  if (typeof apiToken !== "string" || apiToken.length !== 40) {
+    console.log(
+      "Not updating GitHub status as API token is missing or malformed"
+    );
+    return;
+  }
+  const notifications = await getAllNotifications({ token: apiToken });
+  console.log(`Fetched ${notifications.length} notifications`);
+  const numDays = estimateResponseTimeDays(notifications);
+  console.log(`Estimated response time: ${Math.floor(numDays)} days`);
+  const status = getStatus(numDays);
+  console.log("Determining username ...");
+  const username = await getUsername();
+  console.log("Fetching CRSF token for profile status form ...");
+  const crsfToken = await getToken({ username });
+  console.log("Updating GitHub status ...");
+  await setStatus({ token: crsfToken, ...status });
+  console.log("Successfully updated GitHub status ...");
+  const { webhook } = await new Promise((resolve) =>
+    chrome.storage.sync.get(["webhook"], resolve)
+  );
+  if (typeof webhook !== "string" || !webhook) {
+    console.log("Webhook is missing or malformed, skipping ping");
+  } else {
+    await pingWebhook(webhook);
+  }
+}
+
+async function updateStatusMaybe() {
+  if (await shouldRun()) {
+    await updateStatus();
+  }
+}
+
+updateStatusMaybe().catch(console.error);
